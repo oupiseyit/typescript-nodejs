6c5f342250f9b42bca1a9b1c9b5defa1c92652a3
oupiseyit <oupisey.it@gmail.com>
2019-08-18 23:06:25 +0700

    - set up mogoose

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3200;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('<insert your own credentials>', {
+mongoose.connect('mongodb://oupisey:0978927874@cluster0-shard-00-00-jzfln.mongodb.net:27017,cluster0-shard-00-01-jzfln.mongodb.net:27017,cluster0-shard-00-02-jzfln.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
